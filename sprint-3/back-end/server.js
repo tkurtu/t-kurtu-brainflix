d43d98560dac59d42221e058926aa9f25fd7f07d
Tasneem Kurtu <tasneemkurtu@gmail.com>
2019-03-02 11:41:47 -0800

    added json files

--- sprint-3/back-end/server.js
+++ sprint-3/back-end/server.js
@@ -1,21 +1,19 @@
+const fs = require('fs')
 const express = require ('express')
 const cors = require('cors')
 // const bodyParser= require('body-parser')
 const app = express () 
 const videosList = require('./routes/videosList')
-const videoDetails = require('./routes/videoDetails')
-
-
+const videos = require('./routes/videos')
 
 app.use(cors())
-
 // app.use(bodyParser.json())
 
 // This attaches the router to the '/videos' path and array
 // app.use('/videosList', videosList)
 
-app.use('/videoslist', videosList),
-app.use('/videoDetails', videoDetails)
+app.use('/videosList', videosList),
+app.use('/videos', videos)
 
 
 
@@ -25,3 +23,23 @@ app.listen(8080, () => {
 })
 
 
+
+
+// const express = require('express')
+// const router = express.Router()
+// const videosList = require('./videosList')
+// const videoDetailes = require ('./videosDetails')
+
+
+
+// router.get('/', (req, res) => {
+//   res.json()
+// })
+
+// router.get('/', (req, res) => {
+//   res.json()
+// })
+
+
+
+// module.exports = router
